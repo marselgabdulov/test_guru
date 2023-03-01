116f8791b4fcc701860596db9d37b1adb4b627a0
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-01 09:24:16 +0300

    refactor: change user references to author

--- db/migrate/20230228170309_create_tests.rb
+++ db/migrate/20230228170309_create_tests.rb
@@ -3,7 +3,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, null: false, default: 0
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
