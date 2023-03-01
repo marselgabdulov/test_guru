c13c220116ff5a5ef943ec04d76ee57ae4284779
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-01 09:16:45 +0300

    refactor: remove answer - user association

--- db/migrate/20230228170705_create_answers.rb
+++ db/migrate/20230228170705_create_answers.rb
@@ -3,7 +3,6 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
     create_table :answers do |t|
       t.text :body, null: false
       t.boolean :correct, null: false, default: false
-      t.references :user, null: false, foreign_key: true
       t.references :question, null: false, foreign_key: true
 
       t.timestamps
