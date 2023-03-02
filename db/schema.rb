d996bef1c3d4342ec98ebcecde2c862cd4e08e4b
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-02 09:17:17 +0300

    fix: categories migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_28_170705) do
+ActiveRecord::Schema.define(version: 2023_03_02_061524) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2023_02_28_170705) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
