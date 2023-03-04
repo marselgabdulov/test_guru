9d0b1b5cc53219e523e1765c38389c3160f4f779
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-04 10:28:52 +0300

    refactor: remove puts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def user_tests(level)
-    puts Test.joins("JOIN results ON results.test_id = tests.id").where(results: { user_id: id }, level: level)
+    Test.joins("JOIN results ON results.test_id = tests.id")
+        .where(results: { user_id: id }, level: level)
   end
 end
