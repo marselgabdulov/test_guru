bb0362b6f918ca74f405ff5ed48d92ab3d532057
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-04 07:26:15 +0300

    feat: add Test#tests_by_category class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(category_name)
+    joins("JOIN categories ON tests.category_id = categories.id").where(categories: { title: category_name }).order(title: :desc).pluck(:title)
+  end
 end
