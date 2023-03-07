6d0e4ae089d4a08d09ea4a3644c61dfd4d460afe
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-07 17:46:16 +0300

    refactor: User#user_tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :authorized_tests, class_name: "Test", foreign_key: :author_id
 
   def user_tests(level)
-    Test.joins("JOIN results ON results.test_id = tests.id")
-        .where(results: { user_id: id }, level: level)
+    tests.where(level: level)
   end
 end
