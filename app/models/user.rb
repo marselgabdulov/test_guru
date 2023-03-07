5802f55b2fb6febd7e9fbf66a0118f96065cb49a
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-07 11:39:33 +0300

    feat: add author/tests association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
+  has_many :authorized_tests, class_name: "Test", foreign_key: :author_id
 
   def user_tests(level)
     Test.joins("JOIN results ON results.test_id = tests.id")
