cc7ddc6d7ee7150ba7cf55c5436f6ac56efc5a5f
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-07 17:59:39 +0300

    refactor: finish removing results

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_07_144947) do
+ActiveRecord::Schema.define(version: 2023_03_07_082506) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
