dd282b3eea7e08b178f1e22c184bd1fbda363ae7
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-07 11:11:23 +0300

    feat: add questions/test association

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions, dependent: :destroy
 
   def self.tests_by_category(category_name)
     joins("JOIN categories ON tests.category_id = categories.id")
