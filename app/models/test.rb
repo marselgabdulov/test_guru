58879a9762b9b30bf18ef6e294ace59b69dc9755
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-07 11:09:25 +0300

    feat: create tests/category association

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,6 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
   def self.tests_by_category(category_name)
     joins("JOIN categories ON tests.category_id = categories.id")
       .where(categories: { title: category_name })
