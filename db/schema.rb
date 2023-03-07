6c9d004bb2200f4df0092c6bf698a4f0f0924b60
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-07 11:26:55 +0300

    create: model test_passage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_064638) do
+ActiveRecord::Schema.define(version: 2023_03_07_082506) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2023_03_02_064638) do
     t.index ["user_id"], name: "index_results_on_user_id"
   end
 
+  create_table "test_passages", force: :cascade do |t|
+    t.integer "test_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
@@ -68,6 +77,8 @@ ActiveRecord::Schema.define(version: 2023_03_02_064638) do
   add_foreign_key "questions", "tests"
   add_foreign_key "results", "tests"
   add_foreign_key "results", "users"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
 end
