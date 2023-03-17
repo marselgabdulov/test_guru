2ed17820233eb8e02932dfc0d0fd86fd44408455
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-17 09:19:22 +0300

    feat: show action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,23 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: [:index]
+  before_action :find_question, only: [:show]
 
   def index
     render inline: "<ul><% @test.questions.each do |question| %><li><%= question.body %></li><% end %></ul>"
+  end
+
+  def show
+    render inline: "<p><%= @question.body %></p>"
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
