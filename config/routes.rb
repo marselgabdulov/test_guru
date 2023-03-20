e57b7b9d979d48a66106d586a461a32e9daf2756
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-20 21:09:17 +0300

    feat: scaffold answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tests do
-    resources :questions, shallow: true, except: %i[index]
+    resources :questions, shallow: true, except: %i[index] do
+      resources :answers, shallow: true, except: %i[index]
+    end
   end
 end
