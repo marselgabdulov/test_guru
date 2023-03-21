4de4098997ee70c2b85e1757b07c5d5d8fc7fe13
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-21 12:58:33 +0300

    refactor: correct_answer? and before_validation_set_question methods

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,31 +3,29 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :current_question, class_name: "Question", optional: true
 
-  before_validation :before_validation_set_first_question, on: %i[create update]
+  before_validation :before_validation_set_question, on: %i[create update]
 
   def completed?
     current_question.nil?
   end
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
-
-    self.current_question = next_question
+    self.correct_questions += 1 if correct_answer?(answer_ids)
     save!
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_question
+    if current_question.nil?
+      self.current_question = test.questions.first if test.present?
+    else
+      self.current_question = next_question
+    end
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    (correct_answers_count == correct_answers.where[id: answer_ids].count) && correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
