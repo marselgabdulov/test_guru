9f7862a597128253bcac5484c17b2c647e2c56d7
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-21 20:43:14 +0300

    user_authentiation screencast code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
+require "digest/sha1"
+
 class User < ApplicationRecord
+  include Auth
+
+  has_secure_password
+
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :authorized_tests, class_name: "Test", foreign_key: :author_id
-
-  validates :name, presence: true
-  validates :email, presence: true
 
   def user_tests(level)
     tests.where(level: level)
