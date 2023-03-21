d4ca5eed973b7157860085f6797154dc84072919
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-21 19:55:36 +0300

    feat: add password_digest column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_20_202047) do
+ActiveRecord::Schema.define(version: 2023_03_21_165310) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2023_03_20_202047) do
     t.string "role", default: "user", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
