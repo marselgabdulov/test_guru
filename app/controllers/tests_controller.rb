a80f9ae82901061348c55aa1f507e93ee2eb71ab
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-21 19:46:15 +0300

    refactor: mentor's remarks

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,5 @@
 class TestsController < ApplicationController
   before_action :set_test, only: %i[show edit update destroy start]
-  before_action :set_user, only: :start
 
   def index
     @tests = Test.all
@@ -38,15 +37,13 @@ class TestsController < ApplicationController
   end
 
   def start
+    @user = User.first
+
     @user.tests.push(@test)
     redirect_to @user.test_passage(@test)
   end
 
   private
-
-  def set_user
-    @user = User.first
-  end
 
   def set_test
     @test = Test.find(params[:id])
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,7 +5,8 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :current_question, class_name: "Question", optional: true
 
-  before_validation :before_validation_set_question, on: %i[create update]
+  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -34,23 +35,24 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_question
-    if current_question.nil?
-      self.current_question = test.questions.first if test.present?
-    else
-      self.current_question = next_question
-    end
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_question
+    self.current_question = next_question
+  end
+
+  def next_question
+    test.questions.order(:id).where("id > ?", current_question.id).first
   end
 
   def correct_answer?(answer_ids)
+    answer_ids = [] if answer_ids.nil?
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def next_question
-    test.questions.order(:id).where("id > ?", current_question.id).first
   end
 end
