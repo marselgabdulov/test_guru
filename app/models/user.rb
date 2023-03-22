cc2628a0a97516ca94367e751f9aab2cda66870e
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-22 11:11:27 +0300

    feat: add user email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
-
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :authorized_tests, class_name: "Test", foreign_key: :author_id
+
+  has_secure_password
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
 
   def user_tests(level)
     tests.where(level: level)
