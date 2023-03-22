30875f7e71a8a89635baabd743262df0081697a2
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-22 11:18:07 +0300

    feat: add current user when test starts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,8 +20,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
-    @current_user = nil
-    flash[:notice] = "Goodbye!"
     redirect_to login_path
   end
 end
