9452432c99ccd59d98fa20fed41671b12eaf7cb5
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-24 17:39:38 +0300

    localisaton screencast code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locate
 
   protect_from_forgery with: :exception
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   protected
 
@@ -12,5 +17,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+
+  private
+
+  def set_locate
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
