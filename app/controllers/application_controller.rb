70b8132d2c5e68d211700e6bf0d772631c3bfa3f
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-24 10:21:26 +0300

    feat: use devise helper to choose resource dependent by user type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,11 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-
   protected
+
+  def after_sign_in_path_for(resource)
+    current_user.is_a?(Admin) ? admin_tests_path : (stored_location_for(resource) || root_path)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
