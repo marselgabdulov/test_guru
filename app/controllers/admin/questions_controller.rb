7fb9aae13ebd1da27161268d02dc153ecea277b7
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-24 10:09:56 +0300

    refactor: move questions and answers resources to admin

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,6 +1,5 @@
-class QuestionsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_test, only: %i[new create destroy]
+class Admin::QuestionsController < ApplicationController
+  before_action :set_test, only: %i[new create]
   before_action :set_question, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -15,7 +14,7 @@ class QuestionsController < ApplicationController
     @question = @test.questions.new question_params
 
     if @question.save
-      redirect_to @question
+      redirect_to admin_question_path(@question)
     else
       render :new
     end
@@ -25,7 +24,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update question_params
-      redirect_to question_path
+      redirect_to admin_question_path(@question)
     else
       render :edit
     end
@@ -34,7 +33,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
 
-    redirect_to test_url(@test), notice: "Question was destroyed"
+    redirect_to admin_test_url(@question.test), notice: "Question was destroyed"
   end
 
   private
