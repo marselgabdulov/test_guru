c5d26ba7686cce3c6a4119d997d0e34169732e27
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-03-27 10:50:43 +0300

    feat: add link to flash message

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -6,8 +6,7 @@ class GistsController < ApplicationController
 
     flash_options = if @result.success?
                       Gist.create!(gist_params)
-                      { notice: t(".success") }
-
+                      { notice: t(".success") + " #{view_context.link_to(@result.url)}." }
                     else
                       { alert: t(".failure") }
                     end
