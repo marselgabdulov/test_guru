091a194c58539eae9ba4be13dbf960e215acd71b
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-04-03 12:38:33 +0300

    change smtp port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings= {
     address: 'smtp.mail.ru',
-    port: 587,
+    port: 465,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
