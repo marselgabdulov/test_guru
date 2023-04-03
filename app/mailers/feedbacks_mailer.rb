82d919cff94a39eb1c38450c1e20ad275afb6fba
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-04-03 13:37:33 +0300

    fix mistapes

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -2,10 +2,10 @@ class FeedbacksMailer < ApplicationMailer
 
   default to: %{"TestGuru" <admin@testguru.com>}
 
-  def feedbacks_email(fedbacks)
-    @name = fedbacks.name
-    @body = fedbacks.body
-    @email = fedbacks.email
+  def feedbacks_email(feedback)
+    @name = feedback.name
+    @body = feedback.body
+    @email = feedback.email
 
     mail from: @email, subject: 'Contacts email'
   end
