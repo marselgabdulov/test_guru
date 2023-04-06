26307d6bfe16dfbe27080a24a90337989eca4544
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-04-06 10:33:03 +0300

    refactor feddback mailer

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -1,6 +1,4 @@
 class FeedbacksMailer < ApplicationMailer
-  default to: %{"TestGuru <admin@testguru.com>"}
-
   def feedbacks_email(feedback)
     mail to: feedback.email, subject: "Feedback from TestGuru"
   end
