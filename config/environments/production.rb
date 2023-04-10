7b4214b661abd5992109b5730db6a1ba274b3364
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-04-10 09:55:52 +0300

    add tsl to smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options= { host: 'test-guru-id0x.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings= {
+    tls: true,
     address: 'smtp.gmail.com',
     port: 567,
     user_name: ENV['SMTP_USERNAME'],
