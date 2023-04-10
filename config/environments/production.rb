2711b34518fc23b661ce4416b4d07bbdc3e53b71
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-04-10 16:19:23 +0300

    switch to mail.ru

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,10 +65,9 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options= { host: 'test-guru-id0x.onrender.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings= {
-    address: 'smtp.yandex.ru',
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mail.ru',
     port: 465,
-    ssl: true,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
